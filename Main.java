e0f44c336ba200aea03bedf631b4919c936dc5c8
CherniyAleksandr <132821035+CherniyAleksandr@users.noreply.github.com>
2023-07-17 11:14:51 +0200

    Add files via upload
    Commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Main {
+    public static void main (String[] argt){
+        System.out.println("Hello World !");
+
+//      int age = 28;
+//      String name = "Alex";
+//        System.out.println("Возраст" + age + name);
+//        char numCh = 'G';
+//        int num =89;
+//        byte numByte = 4;
+//        short numShort = 56;
+//        float numFloat = 4.7333436f;
+//        double numDouble = 4.355453532;
+//        long numLong = 12121;
+//        System.out.println(num);
+//        System.out.println(numByte);
+//        System.out.println(numShort);
+//        System.out.println(numFloat);
+//        System.out.println(numDouble);
+//        System.out.println(numLong);
+//        System.out.println(numCh);
+
+        int sum = 123 ;
+//        System.out.println(sum/100);
+//        System.out.println((sum /100)%10);
+//        System.out.println(sum %10);
+//
+
+
+    }
+}
